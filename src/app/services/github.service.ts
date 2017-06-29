5006d6c7f13c374cc5da6ff8ad877c12e5472fb4
dakaii <daiki815@gmail.com>
2017-06-29 19:11:53 +0900

    asdlkjfa

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -10,11 +10,20 @@ export class GithubService{
 
     constructor(private _http: Http){
         console.log('Github Service Ready...');
-        this.username = 'dakaii'
     }
 
     getUser(){
         return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
         .map(res => res.json());
     }
+
+    getRepos(){
+        return this._http.get('http://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
+        .map(res => res.json());
+    }
+
+    updateUser(username:string){
+        this.username = username
+    }
+
 }
